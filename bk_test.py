1147eb267f418f5b0b2cac2221e7c28519430128
jsm <john@mightymu.net>
2022-10-04 09:52:58 -0700

    test working

--- bk_test.py
+++ bk_test.py
@@ -1,13 +1,13 @@
 #/usr/bin/env python3
 
-import serial, os, psycopg, time
+import serial, os, psycopg, datetime, time
 from reportlab.pdfgen import canvas
 
-db_connection = psycopg.connect(host='localhost', dbname='jsm', user='jsm', password='Cattail*Unharmed*Clubbing6')
+db_connection = psycopg.connect(host='localhost', dbname='jsm', user='jsm')
 cursor = db_connection.cursor()
 
 def make_pdf():
-	print('Not yet implemented 😿')
+	print('PDF generation is not yet implemented 😿')
 	return
 
 def bk_comm(command):
@@ -17,7 +17,7 @@ def bk_comm(command):
 	bk.write(command_bytes)
 	response = bk.readline()
 	if response:
-	   response_string = response.decode('UTF-8')
+	   response_string = response.decode('UTF-8').strip('\r')
 	   print(f'Response: {response_string}')
 	   return response_string
 	else:
@@ -26,12 +26,10 @@ def bk_comm(command):
 	    return error
 
 def write_to_db(data):
-    cursor.execute('''
-    	INSERT INTO jsm (timestamp, piece, current, Show, length_of_tape) 
-    	VALUES(%s, %s. %s. %s. %s)
-    	'''
-    	(data[0], data[1]. data[2], data[3])
-    cursor.commit()
+    cursor.execute(
+    	'INSERT INTO jsm (timestamp, piece, current, "Show", length_of_tape, voltage) VALUES(%s, %s, %s, %s, %s, %s)',
+    	(datetime.datetime.now(), data[0], data[1], data[2], data[3], data[4]))
+    db_connection.commit()
 
 device = "/dev/BK_1687" #symlinked to whatever the real TTY is
 #Is the PSU plugged in?
@@ -43,27 +41,38 @@ bk = serial.Serial(port = "/dev/BK_1687", baudrate = 9600, timeout = 0.5) #todo:
 bk_comm('SOUT1') #First thing, turn off the PSU output. Turn it back on when ready to start test.
 #If PSU display reads "O P OFF" the last command has succeeded, yay I guess
 
-
+#Begin logging first, while output is off, then turn output on
+def do_test():
+    bk_comm('SOUT0')
+    time.sleep(3)
+    display_values = bk_comm('GETD') #GETD is short for Get Display values. As opposed to (manually-input) settings values.
+    print(f'Response: {display_values}')
+    if int(display_values[0:5]):
+        print("Invalid response. Is PSU on?")
+        pass
+    display_volts = (int(display_values[0:5])/1000)
+    display_curr = (int(display_values[5:-4])/100)
+    #print(bk_response[0:5]) #volts
+    #print(bk_response[4:-5]) #amps
+    print(str(display_values[9]))
+    print(f'Volts: {display_volts}')
+    print(f'Current: {display_curr}')
+    data = ['Wall Panel - 12ft', display_curr, '2022 H&R Block', 12, display_volts]
+    write_to_db(data)
 
 #TODO: print /dev devices, both symlink and target
 #udev rule makes (unfortunately) any CP102x USB-serial device get symlinked as /dev/BK_1687
-bk_comm('SOUT0')
-time.sleep(3)
-display_values = bk_comm('GETD') #GETD is short for Get Display values. As opposed to (manually-input)settings values.
-print(f'Response: {display_values}')
-if type(display_values[0:5]) == int:
-    display_volts = (int(display_values[0:5])/1000)
-else:
-	print('Not a valid response. Is PSU on?')
-display_curr = (int(display_values[5:-5])/100)
-#print(bk_response[0:5]) #volts
-#print(bk_response[5:-5]) #amps
-print(str(display_values[9]))
-print(f'Volts: {display_volts}')
-print(f'Current: {display_curr}')
-data = ['name of piece', display_curr, 'name of show', 'length_of_tape']
+print("Press 't' to begin test")
+control = input()
+if control == 't':
+    start_time = time.time()
+    interval = 3
+    do_test()
+    for i in range(60):
+        time.sleep(interval)
+        do_test()
+    bk_comm('SOUT1')
+    make_pdf()
 
-write_to_db(data)
 
-#TODO: write values to database
 #TODO: create test session, and create results PDF
