a6040ad11c1ed9d00fa350f236baf830115aa23f
jsm <john@mightymu.net>
2023-01-20 08:56:24 -0800

    Add grafana annotations

--- bk_test.py
+++ bk_test.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 
 __author__ = "John Musarra"
 __license__ = "Apache 2.0"
@@ -6,7 +6,8 @@ __email__ = "john@mightymu.net"
 __maintainer__ = "John Musarra"
 __version__ = "alpha"
 
-import serial, os, psycopg, time, requests, sys, random. sqlite
+import serial, os, psycopg, time, requests, sys, random, sqlite3, json
+import constants
 from datetime import datetime, timedelta
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
@@ -24,7 +25,7 @@ def make_pdf(pdf_data):
 
 def bk_comm(command):
     #send command to BK PSU, receive and return response
-    print(f'Sending command: {command}')
+    # print(f'Sending command: {command}')
     command_bytes = bytes(command + '\r', 'ascii') #append a carriage return and encode as bytes
     try:
         bk.write(command_bytes)
@@ -75,44 +76,41 @@ bk_comm('SOUT1') #First thing, turn off the PSU output. Turn it back on when rea
 #Begin logging first, while output is off, then turn output on
 def do_test(piece_name, strip_length, output, show_name, duration, interval): #TODO: Add annotation for beginning of test with piece name and time of day
     print(f'Duration: {duration} seconds')
-    test_begin_time = datetime.now()
+    test_start = datetime.now()
     if output == "on":
         bk_comm('SOUT0')
     else:
         bk_comm('SOUT1')
-    while datetime.now() - test_begin_time <= timedelta(seconds=duration):
+    while datetime.now() - test_start <= timedelta(seconds=duration):
         time.sleep(interval)
         display_values = bk_comm('GETD') #GETD is short for Get Display values. As opposed to (manually-input) settings values.
         assert display_values[0:5], "What now"
-        print(f'Response: {display_values}')
+        # print(f'Response: {display_values}')
         display_volts = (int(display_values[0:5])/1000)
         display_curr = (int(display_values[5:-4])/100)
         print(str(display_values[9]))
+        print('Time Elapsed: [DO THIS]')
         print(f'Volts: {display_volts}')
         print(f'Current: {display_curr}')
         data = [piece_name, display_curr, show_name, strip_length, display_volts]
         write_to_db(data)
     # data = [piece_name, 0, show_name, strip_length, display_volts] - WHY
     test_end_time = datetime.now()
-    return data, test_begin_time, test_end_time
+    return data
 
 def send_annotation(piece_name, test_begin_time, test_end_time, test_id):
-    dashboard_uid = "uz1kXiV4z"
-    panel_id = 1
-    annotation_data = {
-        "dashboardUID": dashboard_uid,
-        "panelID": panel_id,
-        "time": "",
-        "timeEnd": "",
-        "tags": "",
-        "text": f'Piece Name: {piece_name}, Test ID: {test_id}'
-}
-    requests.post('http://localhost:300/api/annotations/', data = annotation_data)
     print("sending grafana annotation...")
+    auth_url = 'http://localhost:3000/'
+    headers = {'Authorization': 'Bearer eyJrIjoiSjA4ZndVR21vSzN1RVZjODZqdHJBTENTY2lqT04wenEiLCJuIjoiYXV0b190ZXN0IiwiaWQiOjF9', 'content-type': 'application/json', 'accept': 'application/json'}
+    #pprint.pprint(headers)
+    dashboard_uid = 'uz1kXiV4z'
+    panel_id = 2
+    annotation_data = {'dashboardUID':dashboard_uid,'time':test_begin_time, 'timeEnd':test_end_time,'text':test_id}
+    response = requests.post('http://localhost:3000/api/annotations', data = json.dumps(annotation_data), headers = headers)
+    print(response.text)
     print(f'Test Begin: {test_begin_time}')
     print(f'Test End: {test_end_time}')
-    #TODO: This
-    pass
+    return response
 
 #TODO: print /dev devices, both symlink and target
 #udev rule makes (unfortunately) any CP102x USB-serial device get symlinked as /dev/BK_1687
@@ -126,13 +124,19 @@ if control == 't':
         piece_name = input()
         print('LED tape length (in decimal feet)?')
         strip_length = input()
+        test_begin = datetime.now().timestamp()
+        test_begin_time = int(test_begin*1000)
+        print(test_begin_time)
         do_test(piece_name, strip_length, output = 'off', show_name = 'testing', duration = 9, interval = 3)
-        do_test(piece_name, strip_length, output = 'on', show_name = 'testing', duration = 125, interval = 5)
+        do_test(piece_name, strip_length, output = 'on', show_name = 'testing', duration = 30, interval = 5)
         do_test(piece_name, strip_length, output = 'off', show_name = 'testing', duration = 10, interval = 3)
-        send_annotation(piece_name, test_begin_time, test_end_time)
+        test_end = datetime.now().timestamp()
+        test_end_time = int(test_end*1000)
+        print(test_end_time)
+        send_annotation(piece_name, test_begin_time, test_end_time, test_id)
         pdf_data = "Time of test: , Test Duration: , Max Current: , Wattage: "
         bk_comm('SOUT1')
-        print(f'Test Duration: {test_duration}')
+        # print(f'Test Duration: {test_duration}')
         make_pdf(pdf_data)
         print("Test Concluded! Hurrah!")
     except KeyboardInterrupt:
